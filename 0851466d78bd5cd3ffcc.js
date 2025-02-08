e84b6e89ea50f3bdce0b3bcc55688838c044d049
arvilays <123064316+arvilays@users.noreply.github.com>
2025-02-07 23:40:34 -0800

    Deployment commit

---
+++ 0851466d78bd5cd3ffcc.js
@@ -0,0 +1,13 @@
+import "../css/reset.css";
+import "../css/style.css";
+import { Task } from "./task.js";
+import { Display } from "./display.js";
+import { Form } from "./form.js";
+
+const main = () => {
+    let display = new Display;
+    let form = new Form;
+    let task = new Task;
+};
+
+main();
